9987f7e09f17842f5900a592ae87613f434279fa
Cody Cook <codycook96@gmail.com>
2019-04-16 18:05:52 +0000

    Adding database for types

---
+++ Source/Database.h
@@ -0,0 +1,29 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <utility>
+
+#include "Type.h"
+
+class Database{
+    private:
+        std::unordered_map<std::string, std::pair<ANY*, TYPE_ID>> data;
+    public:
+        void add(TYPE_ID id, std::string varName);
+
+        template <typename T> 
+        T link(std::string varName){
+            T out = dynamic_cast<T>(data.at(varName).first);
+            if(out == nullptr){
+                std::cout << "Fail" << std::endl;
+                throw "Some Error";
+                return out;
+            }
+            //std::cout << "Good" << std::endl;
+            return out;
+        }
+
+        TYPE_ID type(std::string varName);
+};
+
+#endif
